30db065bf60a3a8a41f942b9aa68feb97196ce53
Programing Kun <phucoding286@gmail.com>
2025-04-20 19:08:42 +0700

    Update modules.py

--- modules.py
+++ modules.py
@@ -22,6 +22,10 @@ def driver_init(chrome_user_data=None, headless=False):
     options = webdriver.ChromeOptions() 
     options.add_argument("--log-level=3")
     options.add_argument("--disable-popup-blocking")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--window-size=1920,1080')
     if headless:
         options.add_argument("--headless")
 
