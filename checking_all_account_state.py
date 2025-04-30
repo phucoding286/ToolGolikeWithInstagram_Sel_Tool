a038841630389a3faa8fe295814d47ffae04017b
Programing Kun <phucoding286@gmail.com>
2025-04-30 23:44:16 +0700

    Add files via upload

--- checking_all_account_state.py
+++ checking_all_account_state.py
@@ -1,4 +1,5 @@
 from modules import *
+from main import load_cookies, storage_cookies
 
 sessions_manager_file = "sessions.json"
 data = json.load(open(sessions_manager_file))['data']
@@ -6,9 +7,11 @@ data = json.load(open(sessions_manager_file))['data']
 for ss_name, ss_path in data.items():
     driver = driver_init(ss_path, False, False)
     driver.set_window_position(500, 0)
+    load_cookies(driver, cookie_f_name=ss_name)
     print(purple_color(f"[A] Account -> {ss_name}"))
     driver.get("https://www.instagram.com/")
     input(system_color("[>] Nhấn enter để tiếp tục\n-> "))
+    storage_cookies(driver, cookie_f_name=ss_name)
     driver.quit()
 
 input(success_color("[>] Bạn đã lặp qua hết tất cả các session rồi, enter để đóng\n-> "))
